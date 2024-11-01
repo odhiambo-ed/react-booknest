89d39d0f35607aa8b07a7e8cbea3ac27feac37c0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-01 09:08:03 +0300

    wrap book provider to root

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.js'
+import { BookProvider } from './context/BookContext.js'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BookProvider>
+        <App />
+      </BookProvider>
     </ThemeProvider>
   </StrictMode>
 );
